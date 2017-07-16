885931115b558898dc63d4341fa9b208f8b661d7
Eyad Alshafi <eyad.alshafi@gmail.com>
2017-07-16 16:32:18 -0500

    version 1.0

--- score.js
+++ score.js
@@ -2,9 +2,11 @@ var p1Button = document.getElementById("p1");
 var p2Button = document.getElementById("p2");
 var p1Text = document.getElementById("p1text");
 var p2Text = document.getElementById("p2text");
+var numInput = document.querySelector("input");
+var playingTo = document.getElementById("playingto");
 var p1Score = 0;
 var p2Score = 0;
-var reset = document.getElementById("reset");
+var resetButton = document.getElementById("resetbutton");
 var gameOver = false;
 var max = 5;
 
@@ -13,6 +15,7 @@ p1Button.addEventListener("click", function() {
 			p1Score++;
 			if (p1Score === max) {
 				gameOver = true;
+				p1Text.classList.add("green");
 			}
 			p1Text.textContent = p1Score;
 	}
@@ -23,15 +26,29 @@ p2Button.addEventListener("click", function() {
 		p2Score++;
 		if (p2Score === max) {
 			gameOver = true;
+			p2Text.classList.add("green");
 		}
 			p2Text.textContent = p2Score;
 	}
 });
 
-reset.addEventListener("click", function() {
+resetButton.addEventListener("click", function() {
+	reset();
+});
+
+function reset() {
 	p1Score = 0;
 	p2Score = 0;
 	p1text.textContent = 0;
 	p2text.textContent = 0;
 	gameOver = false;
+	p1Text.classList.remove("green");
+	p2Text.classList.remove("green");
+	gameOver = false;
+}
+
+numInput.addEventListener("change", function() {
+	playingTo.textContent = this.value;
+	max = Number(this.value);
+	reset();
 });
